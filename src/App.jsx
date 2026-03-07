289260c6d56b0fcd98e7001e812d0bd130e8d99c
abdelrhman-mostafa95 <abdelrhman.mostafa1095@gmail.com>
2026-03-07 21:04:42 +0200

    feat: enhance animations and update styles for improved visual effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import AppRouter from './app/router/AppRouter'
 import { ThemeProvider } from './app/providers/ThemeProvider'
+import { MotionConfig } from 'framer-motion'
 
 function App() {
   return (
     <ThemeProvider>
-      <AppRouter />
+      <MotionConfig reducedMotion="never">
+        <AppRouter />
+      </MotionConfig>
     </ThemeProvider>
   )
 }
